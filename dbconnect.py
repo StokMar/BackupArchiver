fc02efbb5bb0ca24774467f80fb6737c2c85f1b1
StokMar <stokic@bphh.de>
2016-09-19 17:00:11 +0200

    connector testing

--- dbconnect.py
+++ dbconnect.py
@@ -1,14 +1,29 @@
-import mysql
+import MySQLdb
 import getpass
 import os
 import _strptime
 
+from pip._vendor.distlib import database
 
-def connect ():
+
+def connect():
+    print "Enter Host"
+    host = raw_input()
+
+    print "Enter Database"
+    database = raw_input()
+
     print "Enter User"
     user = raw_input()
 
     print "Enter Password"
-    password = raw_input(getpass.getpass)
+    password = getpass.getpass()
+
+
+
+
+
+ MySQLdb.connect(host=host, database=database, user=user, password=password)
+
 
 connect()
